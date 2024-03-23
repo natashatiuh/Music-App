c0a2d2ca2d9f073909fa4cd0a07d872c5dc9c01b
Наталія Тюх <nataliatuh@Natalias-MacBook-Air.local>
2024-03-23 18:11:10 -0400

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("Hello")
+import express from "express"
+import cors from "cors"
+
+async function main() {
+    const app = express()
+    const port = 4000
+
+    app.use(express.json())
+    app.use(cors())
+
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`)
+    })
+}
+
+main()
